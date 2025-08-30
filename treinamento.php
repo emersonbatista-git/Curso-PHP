a42498719dd967a335c7ded55bd186583783a51a
Emerson Santos <ebsantoss@gmail.com>
2025-08-30 11:44:51 -0300

    estudos

---
+++ treinamento.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php 
+
+$a = 14;
+$b = 2;
+
+
+echo"A soma de $a e $b é " . ($r = $a + $b); 
+echo"<br>A Multiplicação de $a e $b é " . ($r = $a * $b);
+echo"<br>A Divisão de $a por $b é " . ($r = $a / $b);
+echo "<br>A média entre $a e $b é " . ($m = ($a + $b)/2);
+echo "<br>A potencia de $a elevado a $b é " . ($p = pow($a, $b));
+echo"<br>A é maior que B: ". (($s = $a > $b) ? "Verdadeiro":"Falso"); 
+
+
+
+
+?>
+    
+</body>
+</html>
