55efd8fac1ee3b1440cc8d18fa4d1ee72d81c538
Emerson Santos <ebsantoss@gmail.com>
2025-09-03 17:44:05 -0300

    ok

--- aula09/desafio.php
+++ aula09/desafio.php
@@ -3,9 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+
+<div>
+    <?php 
+         $n1 = $_GET ["nt1"];
+         $n2 = $_GET ["nt2"];
+         $m = ($n1 + $n2) / 2;
+         
+
+        if ($m <5) {
+            $sit= "REPROVADO";
+            
+            
+        }
+
+        elseif ($m >= 5 && $m <7) {
+            
+            $sit = "RECUPERAÇÃO";
+            
+            
+           
+
+        }
+            else {
+                $sit = "APROVADO";
+               
+            }
+             
+
+
+        
+
+        echo "A média entre " . number_format($n1,1);;
+        echo " e "  . number_format($n2,1);;
+        echo " é igual a " . number_format($m,1);;
+        echo "<br/>Situação do aluno: $sit";
+
+   
+    
+    
+    ?>
+    
+   
+         
+</div>
+<a href="desafio.html">Voltar</a>
+
+
+
     
 </body>
 </html>
