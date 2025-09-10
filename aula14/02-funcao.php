787b61134a3bc1e7d8181310c36ba9bd31ae3a66
Emerson Santos <ebsantoss@gmail.com>
2025-09-10 14:05:17 -0300

    aula14

--- aula14/02-funcao.php
+++ aula14/02-funcao.php
@@ -9,7 +9,7 @@
     <div>
         <?php
                  function soma() { 
-                    $p = func_get_args();
+                     $p = func_get_args();
                      $t = func_num_args();
                      $s =0;
                        for ($i=0; $i < $t; $i++) {
